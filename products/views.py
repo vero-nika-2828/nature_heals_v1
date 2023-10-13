7e63f2140f4e218d0eede02cf29b4c2490e5462c
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-10-13 12:14:07 +0000

    Add product_details functionality in products/views.py

--- products/views.py
+++ products/views.py
@@ -63,3 +63,14 @@ def all_products(request):
 
     }
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product-details.html', context)
