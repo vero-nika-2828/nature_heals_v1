87e09ffd6774ad8f958785751951c9e84acf0db0
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-10-22 15:39:45 +0000

    Update CheckoutConfig class in checkout/apps.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    """
+    Override ready method to import signals and 
+    Call update total model each time  line item is deleted or saved
+    """
+
+    def ready(self):
+        import checkout.signals
