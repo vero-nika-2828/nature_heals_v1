b6f3b0c47a686bc8766b5b6fefb0c8ad3400a023
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-12-16 13:10:53 +0000

    Add wishlist function in views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -4,11 +4,25 @@ from django.contrib import messages
 
 from profiles.models import UserProfile
 from products.models import Product
+from .models import Wishlist
 # from wishlist.models import WishList
 
 
 def wishlist(request):
+    """
+    Display my_wishlist page with the items selected by the user
+    """
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.filter(user_profile=user)
 
     template = 'wishlist/my_wishlist.html'
 
-    return render(request, template)
+    context={
+        'wishlist': wishlist,
+
+    }
+    print(user)
+    print(wishlist)
+        
+
+    return render(request, template, context)
