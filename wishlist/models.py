7b67884336b2a34d4aaee0d8a91ed2dd33a0d50c
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-12-17 19:14:13 +0000

    Fix wishlist function in products/views.py

--- wishlist/models.py
+++ wishlist/models.py
@@ -12,4 +12,4 @@ class Wishlist(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'WishList ({self.user_profile})'
+        return f'{self.user_profile} : {self.product}'
