983eac5e378d60327eb6cfcb6c2c082d2ae83b63
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-05 00:50:25 +0000

    Update styling in toast messages

--- bag/views.py
+++ bag/views.py
@@ -23,8 +23,8 @@ def add_to_bag(request, item_id):
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
         messages.success(
-            request,
-            'Updated {product.friendly_name} quantity to {bag[item_id]}')
+            request, 
+            f'Updated {product.friendly_name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.friendly_name} to your bag')
