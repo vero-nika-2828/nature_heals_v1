59d218e2d7b6199ca6622827dbabc96d6587b77b
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-10 18:40:42 +0000

    Add exclude call to popular_prodcuts filter

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 def index(request):
     """Returns index page"""
-    popular_products = Product.objects.filter(featured=True).order_by('?')[:4]
+    popular_products = Product.objects.filter(featured=True).exclude(image='').order_by('?')[:4]
 
     context = {
         'popular_products': popular_products,
