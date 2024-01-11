3b5563e13db63f1a0db2c1a38eaf3ac1e15a610a
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-11 20:17:32 +0000

    Set debug to false for deployment

--- nature_heals/settings.py
+++ nature_heals/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fj5&b2*lw+_yq-)odqf@reag%@l&svhx$$@qhuycl)$=4)b07t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nature-heals.herokuapp.com', 'localhost']
 
