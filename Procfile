3973d0fa7d452e0d3f7b65bceb3fa41a259a8b16
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-11 12:54:45 +0000

    Deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nature_heals.wsgi:application
--- nature_heals/settings.py
+++ nature_heals/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fj5&b2*lw+_yq-)odqf@reag%@l&svhx$$@qhuycl)$=4)b07t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-veronika282-natureheals-2dp854eh6xv.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['vn2810902-nature-heals.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,22 @@ WSGI_APPLICATION = 'nature_heals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE' in os.environ:
+    DATABASES = {
+         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+         }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
     }
-}
+
+
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Password validation
