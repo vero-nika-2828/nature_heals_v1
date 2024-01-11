dfeea216c30a6994377d06529e608612993b9276
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-11 12:57:34 +0000

    Deploy to heroku2

--- nature_heals/settings.py
+++ nature_heals/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fj5&b2*lw+_yq-)odqf@reag%@l&svhx$$@qhuycl)$=4)b07t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vn2810902-nature-heals.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['vnnh2810-nature-heals.herokuapp.com', 'localhost']
 
 
 # Application definition
