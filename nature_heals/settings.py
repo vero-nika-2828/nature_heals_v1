cb5b99d3d5f10ed8ae609d59aa6ffc93de554913
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-17 22:58:08 +0000

    Update secret_key in settings.py

--- nature_heals/settings.py
+++ nature_heals/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY='sznea$2jw_judae+#g+l3h245+(o*26pm0nz^*23)t8r311$y+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
