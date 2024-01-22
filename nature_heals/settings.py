4cd94cc7d0ad0e089220cc36acb404572e1b1408
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-22 22:42:48 +0000

    Save real email settings in setting.py

--- nature_heals/settings.py
+++ nature_heals/settings.py
@@ -248,7 +248,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'naturehealso@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
