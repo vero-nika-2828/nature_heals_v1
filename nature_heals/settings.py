9860f4cc0fc1c936b5b010350b1c0b76926b98e3
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-23 22:53:51 +0000

    Add image info in product.json

--- nature_heals/settings.py
+++ nature_heals/settings.py
@@ -148,6 +148,9 @@ else:
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
