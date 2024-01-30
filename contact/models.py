e33f4818648e7da0a9c52b7fbbe31baf93a47cfc
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-01-30 20:30:19 +0000

    Add modules in contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    """
+    Contact model to submit customer's questions
+    """
+
+    name = models.CharField(max_length=35, null=True, blank=True)
+    email =  models.EmailField(max_length=254)
+    message = models.TextField(max_length=254)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Message from {self.name}'
+
+
