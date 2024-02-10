e672eb72ac1e3151f7dc0051464c772225aaeb4a
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2024-02-10 14:45:19 +0000

    Update checkout success confirmation message in views.py

--- checkout/views.py
+++ checkout/views.py
@@ -160,8 +160,10 @@ def checkout_success(request, order_number):
                 user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        Your order number is: \
+        {order_number}.\
+        A confirmation email will be sent to\
+        {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
